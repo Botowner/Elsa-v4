57e0fa1226ea6b8c6a1ecc3f1e121f5c0fcc9de6
Aswin <100590136+Botowner@users.noreply.github.com>
2022-07-25 00:32:27 +0530

    Update autofilterPM.py

--- LuciferMoringstar_Robot/modules/autofilterPM.py
+++ LuciferMoringstar_Robot/modules/autofilterPM.py
@@ -50,7 +50,7 @@ async def pm_filters(client, update):
                     )
                 else:
                     btn.append(
-                        [InlineKeyboardButton(text=f"{filesize}", callback_data=f"luciferPM#{file_id}"),
+                        [InlineKeyboardButton(text=f"▣{filesize}➣", callback_data=f"luciferPM#{file_id}"),
                          InlineKeyboardButton(text=f"{filename}", callback_data=f"luciferPM#{file_id}")]
                     )
         else:
@@ -69,8 +69,8 @@ async def pm_filters(client, update):
         else:
             buttons = btn
             buttons.append(
-                [InlineKeyboardButton(text="📃 Pages 1/1",callback_data="pages"),
-                 InlineKeyboardButton("Close 🗑️", callback_data="close")]
+                [InlineKeyboardButton(text="ᴘᴀɢᴇ 1/1",callback_data="pages"),
+                 InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")]
             )
 
 
@@ -88,9 +88,9 @@ async def pm_filters(client, update):
         buttons = data['buttons'][0].copy()
     
         buttons.append(
-            [InlineKeyboardButton(text=f"📃 1/{data['total']}",callback_data="pages"),
-             InlineKeyboardButton("🗑️", callback_data="close"),
-             InlineKeyboardButton(text="➡",callback_data=f"nextbot_0_{keyword}")]
+            [InlineKeyboardButton(text=f"ᴘᴀɢᴇ 1/{data['total']}",callback_data="pages"),
+             InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close"),
+             InlineKeyboardButton(text="ɴᴇxᴛ",callback_data=f"nextbot_0_{keyword}")]
         )
         
         if REQUEST_MOVIE:
